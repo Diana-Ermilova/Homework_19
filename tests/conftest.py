2ec91c5175c953abff6d729d176dd19dc7efe48a
diana.ermilova <diana.ermilova.96@gmail.com>
2025-05-28 00:15:08 +0300

    Try to test with browserstack

---
+++ tests/conftest.py
@@ -0,0 +1,59 @@
+import pytest
+from appium.options.android import UiAutomator2Options
+from appium.options.ios import XCUITestOptions
+from selene import browser
+import os
+from appium import webdriver
+import allure_commons
+from selene import browser, support
+from config import config
+
+@pytest.fixture(scope='function')
+def android_management():
+    options = UiAutomator2Options().load_capabilities({
+        "platformVersion": "9.0",
+        "deviceName": "Google Pixel 3",
+        "app": config.app, # по умолчанию запускается википедия?
+
+        'bstack:options': {
+            "projectName": "QA Guru Homework Project",
+            "buildName": "browserstack-build-1",
+            "sessionName": "BStack first_test",
+
+            "userName": config.username,
+            "accessKey": config.accessKey}
+    })
+    browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+    browser.config.timeout = float(os.getenv('timeout', '10.0'))
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext
+    )
+    session_id = browser.driver.session_id
+    yield
+    browser.quit()
+
+@pytest.fixture(scope='function')
+def ios_management():
+    options = XCUITestOptions().load_capabilities({
+        "platformName": "ios",
+        "platformVersion": "13",
+        "deviceName": "iPhone 11",
+        "app": config.app,
+
+        'bstack:options': {
+            "projectName": "QA Guru Homework Project",
+            "buildName": "browserstack-build-1",
+            "sessionName": "BStack first_test",
+
+            "userName":  config.username,
+            "accessKey": config.accessKey}
+    })
+
+    browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+    browser.config.timeout = float(os.getenv('timeout', '10.0'))
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext
+    )
+    session_id = browser.driver.session_id
+    yield
+    browser.quit()
